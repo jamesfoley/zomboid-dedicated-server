3fe91cea45f4c1186563f5778e44dc503f7724d1
James Foley <jamesrichmondfoley@googlemail.com>
2022-01-08 13:05:46 +0000

    Remove some stuff I don't really need

--- src/run_server.sh
+++ src/run_server.sh
@@ -21,81 +21,6 @@ function start_server() {
         -steamvac "$STEAM_VAC" "$USE_STEAM"
 }
 
-function apply_postinstall_config() {
-    printf "\n### Applying Post Install Configuration...\n"
-
-    # Set the Server Publicity status
-    sed -i "s/Open=.*/Open=$PUBLIC_SERVER/g" "$SERVER_CONFIG"
-
-    # Set the Server query Port
-    sed -i "s/DefaultPort=.*/DefaultPort=$QUERY_PORT/g" "$SERVER_CONFIG"
-
-    # Set the Server game Port
-    sed -i "s/SteamPort1=.*/SteamPort1=$GAME_PORT/g" "$SERVER_CONFIG"
-
-    # Set the Server Name
-    sed -i "s/PublicName=.*/PublicName=$SERVER_NAME/g" "$SERVER_CONFIG"
-
-    # Set the Server Password
-    sed -i "s/Password=.*/Password=$SERVER_PASSWORD/g" "$SERVER_CONFIG"
-
-    # Set the Mod names
-    sed -i "s/Mods=.*/Mods=$MOD_NAMES/g" "$SERVER_CONFIG"
-
-    # Set the Mod Workshop IDs
-    sed -i "s/WorkshopItems=.*/WorkshopItems=$MOD_WORKSHOP_IDS/g" "$SERVER_CONFIG"
-
-    # Set the Server PvP status
-    sed -i "s/PVP=.*/PVP=$SERVER_PVP/g" "$SERVER_CONFIG"
-
-    # Set the Autosave Interval
-    sed -i "s/SaveWorldEveryMinutes=.*/SaveWorldEveryMinutes=$AUTOSAVE_INTERVAL/g" "$SERVER_CONFIG"
-
-    # Set the Car Spawn Rate
-    sed -i "s/CarSpawnRate = .*/CarSpawnRate = $CAR_SPAWN_RATE,/g" "$SERVER_RULES_CONFIG"
-
-    # Set the Max Players
-    sed -i "s/MaxPlayers=.*/MaxPlayers=$MAX_PLAYERS/g" "$SERVER_CONFIG"
-
-    # Set the maximum amount of RAM for the JVM
-    sed -i "s/-Xmx.*/-Xmx$MAX_RAM\",/g" "$SERVER_VM_CONFIG"
-
-    # Set the Pause on Empty Server
-    sed -i "s/PauseEmpty=.*/PauseEmpty=$PAUSE_ON_EMPTY/g" "$SERVER_CONFIG"
-
-    # Set the Player Safehouse
-    sed -i "s/PlayerSafehouse=.*/PlayerSafehouse=$PLAYER_SAFEHOUSE/g" "$SERVER_CONFIG"
-
-    # Set the Player Safehouse Respawn
-    sed -i "s/SafehouseAllowRespawn=.*/SafehouseAllowRespawn=$SAFEHOUSE_RESPAWN/g" "$SERVER_CONFIG"
-
-    # Set the Sleep Until Morning
-    sed -i "s/SleepAllowed=.*/SleepAllowed=$SLEEP_ALLOWED/g" "$SERVER_CONFIG"
-
-    # Set the Spawn with Starter Kit
-    sed -i "s/StarterKit = .*/StarterKit = $STARTER_KIT,/g" "$SERVER_RULES_CONFIG"
-
-    # Set the Weapon Multi Hit
-    sed -i "s/MultiHitZombies = .*/MultiHitZombies = $WEAPON_MULTI_HIT,/g" "$SERVER_RULES_CONFIG"
-
-    printf "\n### Post Install Configuration applied.\n"
-}
-
-# Test if this is the the first time the server has run
-function test_first_run() {
-    printf "\n### Checking if this is the first run...\n"
-
-    if [[ ! -f "$SERVER_CONFIG" ]] || [[ ! -f "$SERVER_RULES_CONFIG" ]]; then
-        printf "\n### This is the first run.\nStarting server for %s seconds\n" "$TIMEOUT"
-        start_server
-        TIMEOUT=0
-    else
-        printf "\n### This is not the first run.\n"
-        TIMEOUT=0
-    fi
-
-    printf "\n### First run check complete.\n"
-}
 
 # Update the server
 function update_server() {
@@ -147,20 +72,11 @@ function set_variables() {
     # Set the game version variable
     GAME_VERSION=${GAME_VERSION:-"public"}
 
-    # Set the Server Publicity variable
-    PUBLIC_SERVER=${PUBLIC_SERVER:-"true"}
-
     # Set the IP Query Port variable
     QUERY_PORT=${QUERY_PORT:-"16261"}
 
-    # Set the IP Game Port variable
-    GAME_PORT=${GAME_PORT:-"8766"}
-
     # Set the Server name variable
     SERVER_NAME=${SERVER_NAME:-"ZomboidServer"}
-
-    # Set the Server Password variable
-    SERVER_PASSWORD=${SERVER_PASSWORD:-""}
 
     # Set the Server Admin Password variable
     ADMIN_USERNAME=${ADMIN_USERNAME:-"admin"}
@@ -177,54 +93,11 @@ function set_variables() {
 
     # Set Steam VAC Protection variable
     STEAM_VAC=${STEAM_VAC:-"true"}
-
-    # Set the Mods to use from workshop
-    MOD_NAMES=${MOD_NAMES:-""}
-    MOD_WORKSHOP_IDS=${MOD_WORKSHOP_IDS:-""}
-
-    # Set PVP variable
-    SERVER_PVP=${SERVER_PVP:-"true"}
-
-    # Set the Autosave Interval variable
-    AUTOSAVE_INTERVAL=${AUTOSAVE_INTERVAL:-"15"}
-
-    # Set the Car Spawn rate variable
-    CAR_SPAWN_RATE=${CAR_SPAWN_RATE:-"3"}
-
-    # Set the Max Players variable
-    MAX_PLAYERS=${MAX_PLAYERS:-"16"}
-
-    # Set the Maximum RAM variable
-    MAX_RAM=${MAX_RAM:-"4096m"}
-
-    # Set the Pause on Empty variable
-    PAUSE_ON_EMPTY=${PAUSE_ON_EMPTY:-"true"}
-
-    # Set Player Safehouse variable
-    PLAYER_SAFEHOUSE=${PLAYER_SAFEHOUSE:-"true"}
-
-    # Set the Player Safehouse Respawn variable
-    SAFEHOUSE_RESPAWN=${SAFEHOUSE_RESPAWN:-"true"}
-
-    # Set the Sleep until morning variable
-    SLEEP_ALLOWED=${SLEEP_ALLOWED:-"true"}
-
-    # Set the Starter Kit variable
-    STARTER_KIT=${STARTER_KIT:-"true"}
-
-    # Set the Weapon Multi-Hit variable
-    WEAPON_MULTI_HIT=${WEAPON_MULTI_HIT:-"true"}
-
-    SERVER_CONFIG="$CONFIG_DIR/Server/$SERVER_NAME.ini"
-    SERVER_VM_CONFIG="$BASE_GAME_DIR/ProjectZomboid64.json"
-    SERVER_RULES_CONFIG="$CONFIG_DIR/Server/${SERVER_NAME}_SandboxVars.lua"
 }
 
 ## Main
 set_variables
 update_folder_permissions
-apply_preinstall_config
 update_server
-test_first_run
 apply_postinstall_config
 start_server
